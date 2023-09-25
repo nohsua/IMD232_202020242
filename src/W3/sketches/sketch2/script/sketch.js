7638b3b93b4afa0664d0ee05e638159302b35117
nohsua <129596735+nohsua@users.noreply.github.com>
2023-09-25 19:13:36 +0900

    3주차 추가

---
+++ src/W3/sketches/sketch2/script/sketch.js
@@ -0,0 +1,55 @@
+let pos;
+let vel;
+let acc;
+let radius = 25;
+
+function setup() {
+  setCanvasContainer('mySketchGoesHere', 3, 2, true);
+  background('255');
+  pos = createVector(width / 2, height / 2);
+  vel = createVector(0, 0);
+  acc = createVector();
+  console.log(pos);
+  console.log(vel);
+  ellipse(pos.x, pos.y, 50);
+}
+
+function draw() {
+  background('255');
+  update();
+  infiniteEdge();
+  ellipse(pos.x, pos.y, 2 * radius);
+}
+
+function update() {
+  acc = p5.Vector.random2D();
+  acc.mult(0.5);
+  vel.add(acc);
+  vel.limit(5);
+  pos.add(vel);
+}
+
+function infiniteEdge() {
+  if (pos.x < 0) {
+    pos.x = width;
+  } else if (pos.x > width) {
+    pos.x = 0;
+  }
+  if (pos.y < 0) {
+    pos.y = height;
+  } else if (pos.y > height) {
+    pos.y = 0;
+  }
+}
+// if(pos.x < 0){
+//  vel.x *= -1;
+//  } else if (pos.x > width) {
+//    vel.x *= -1;
+//}
+
+//if (pos.x - radius < 0 || pos.x + radius > width) {
+//  vel.x *= -1;
+//}
+//if (pos.y - radius < 0 || pos.y + radius > height) {
+//  vel.y *= -1;
+//}
